7ea1a9fe0b09ec4de5f3a8545402f1fbaa0d31db
Aimable01 <aimable.kwizera14@gmail.com>
2025-03-07 09:24:40 +0200

    feat: web

--- calc/truffle-back-cop/contracts/Calculator.sol
+++ calc/truffle-back-cop/contracts/Calculator.sol
@@ -2,19 +2,24 @@
 pragma solidity ^0.8.19;
 
 contract Calculator{
+
     uint public result;
+    
     function add(uint num1, uint num2) public returns(uint){
         result = num1 + num2;
         return result;
     }
+
     function sub(uint num1, uint num2) public returns(uint){
         result = num1 - num2;
         return result;
     }
+
     function mul(uint num1, uint num2) public returns(uint){
         result = num1 * num2;
         return result;
     }
+
     function div(uint num1, uint num2) public returns(uint){
         result = num1 / num2;
         return result;
--- calc/truffle-back-cop/migrations/2_deploy_contract.js
+++ calc/truffle-back-cop/migrations/2_deploy_contract.js
@@ -1,5 +1,7 @@
-const HelloWorld = artifacts.require("HelloWorld");
+// const HelloWorld = artifacts.require("HelloWorld");
+const Calculator = artifacts.require("Caculator");
 
 module.exports = function (deployer) {
-  deployer.deploy(HelloWorld);
+  // deployer.deploy(HelloWorld);
+  deployer.deploy(Calculator);
 };
