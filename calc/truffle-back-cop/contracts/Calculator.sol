718a85af6bf5c30bd5d0817424b0ee13e54564b4
Aimable01 <aimable.kwizera14@gmail.com>
2025-03-07 09:03:00 +0200

    feat: calc contract

---
+++ calc/truffle-back-cop/contracts/Calculator.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Calculator{
+    uint public result;
+    function add(uint num1, uint num2) public returns(uint){
+        result = num1 + num2;
+        return result;
+    }
+    function sub(uint num1, uint num2) public returns(uint){
+        result = num1 - num2;
+        return result;
+    }
+    function mul(uint num1, uint num2) public returns(uint){
+        result = num1 * num2;
+        return result;
+    }
+    function div(uint num1, uint num2) public returns(uint){
+        result = num1 / num2;
+        return result;
+    }
+}
